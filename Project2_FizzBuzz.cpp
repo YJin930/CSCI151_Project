fe76f24c8ea290d56cc0e3d155bcd0b89b635b45
YJin930 <92229987+YJin930@users.noreply.github.com>
2021-11-05 18:31:05 -0700

    Add files via upload

---
+++ Project2_FizzBuzz.cpp
@@ -0,0 +1,29 @@
+//Yuanjin Liang
+//Project 2: Fizz Buzz
+//October 17th 2021
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    for (int i = 1; i < 101; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            std::cout << "FizzBuzz\n";
+        }
+        else if (i % 3 == 0)
+        {
+            std::cout << "Fizz\n";
+        }
+        else if (i % 5 == 0)
+        {
+            std::cout << "Buzz\n";
+        }
+        else
+        {
+            std::cout << i << "\n";
+        }
+    }
+}
